9d7d08132fbe30d5c38b37a1c9c50c87eab9258d
Luiz Chagas <luizchagasjr@gmail.com>
2017-09-08 12:18:02 -0500

    big fixes

--- app/public/assets/js/default.js
+++ app/public/assets/js/default.js
@@ -1,6 +1,6 @@
 $(window).load(function() {
   // Animate loader off screen
-  $(".se-pre-con").fadeOut("slow");;
+  $(".se-pre-con").fadeOut("slow");
 });
 $(document).ready(function(){
   $('body').scrollspy({
@@ -38,27 +38,27 @@ new WOW({
 }).init();
 
 // Back to Top
-// if ($('#back-to-top').length) {
-//     var scrollTrigger = 100, // px
-//         backToTop = function () {
-//             var scrollTop = $(window).scrollTop();
-//             if (scrollTop > scrollTrigger) {
-//                 $('#back-to-top').addClass('show');
-//             } else {
-//                 $('#back-to-top').removeClass('show');
-//             }
-//         };
-//     backToTop();
-//     $(window).on('scroll', function () {
-//         backToTop();
-//     });
-//     $('#back-to-top').on('click', function (e) {
-//         e.preventDefault();
-//         $('html,body').animate({
-//             scrollTop: 0
-//         }, 700);
-//     });
-// }
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
 
 // Gallery
 document.getElementById('gallery').onclick = function (event) {
